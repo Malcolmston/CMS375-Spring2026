062a7c366c7fd6f187eae65a5f64cae875f8d271
Malcolmston <mstone@crefeld.org>
2026-02-22 21:35:07 -0500

    feat: add `trg_log_user_soft_delete` trigger to log user deletions with old and new data
    As a note, this trigger will only log the soft deletion of user information

--- sql/trigger.sql
+++ sql/trigger.sql
@@ -53,3 +53,36 @@ AFTER UPDATE ON users
     END IF;
 END;
 
+DROP TRIGGER IF EXISTS trg_log_user_soft_delete;
+
+CREATE TRIGGER trg_log_user_soft_delete
+AFTER UPDATE ON users
+    FOR EACH ROW BEGIN
+    IF NOT (OLD.deleted_at <=> NEW.deleted_at) THEN
+        INSERT INTO logs (user_id, action, table_name, record_id, old_data, new_data)
+        VALUES (
+                   NEW.id,
+                   'DELETE',
+                   'users',
+                   NEW.id,
+                   JSON_OBJECT(
+                           'firstname',  OLD.firstname,
+                           'lastname',   OLD.lastname,
+                           'email',      OLD.email,
+                           'age',        OLD.age,
+                           'blood',      OLD.blood,
+                           'gender',     OLD.gender,
+                           'deleted_at', OLD.deleted_at
+                   ),
+                   JSON_OBJECT(
+                           'firstname',  NEW.firstname,
+                           'lastname',   NEW.lastname,
+                           'email',      NEW.email,
+                           'age',        NEW.age,
+                           'blood',      NEW.blood,
+                           'gender',     NEW.gender,
+                           'deleted_at', NEW.deleted_at
+                   )
+               );
+    END IF;
+END;
