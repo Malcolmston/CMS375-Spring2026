949c41525b29561cac6a26144ae032c672dcbad1
Malcolmston <mstone@crefeld.org>
2026-02-22 21:38:50 -0500

    feat: add `trg_log_user_recover` trigger to log user recoveries with old and new data

--- sql/trigger.sql
+++ sql/trigger.sql
@@ -86,3 +86,37 @@ AFTER UPDATE ON users
                );
     END IF;
 END;
+
+DROP TRIGGER IF EXISTS trg_log_user_recover;
+
+CREATE TRIGGER trg_log_user_recover
+AFTER UPDATE ON users
+    FOR EACH ROW BEGIN
+    IF OLD.deleted_at IS NOT NULL AND NEW.deleted_at IS NULL THEN
+        INSERT INTO logs (user_id, action, table_name, record_id, old_data, new_data)
+        VALUES (
+                   NEW.id,
+                   'RECOVER',
+                   'users',
+                   NEW.id,
+                   JSON_OBJECT(
+                           'firstname',  OLD.firstname,
+                           'lastname',   OLD.lastname,
+                           'email',      OLD.email,
+                           'age',        OLD.age,
+                           'blood',      OLD.blood,
+                           'gender',     OLD.gender,
+                           'deleted_at', OLD.deleted_at
+                   ),
+                   JSON_OBJECT(
+                           'firstname',  NEW.firstname,
+                           'lastname',   NEW.lastname,
+                           'email',      NEW.email,
+                           'age',        NEW.age,
+                           'blood',      NEW.blood,
+                           'gender',     NEW.gender,
+                           'deleted_at', NEW.deleted_at
+                   )
+               );
+    END IF;
+END;
