ec8ee78715668b1597c9a80e49b1a0c5bca31bd4
Malcolmston <mstone@crefeld.org>
2026-02-22 20:45:36 -0500

    fix: adjust `get_user` procedure to correctly handle `is_deleted` parameter and switch views based on status

--- sql/procedure.sql
+++ sql/procedure.sql
@@ -5,22 +5,22 @@ CREATE PROCEDURE get_user(
     OUT user_name  VARCHAR(255),
     OUT user_email VARCHAR(255),
     OUT user_phone VARCHAR(255),
-    OUT is_deleted BOOLEAN
+    INOUT is_deleted BOOLEAN
 )
 BEGIN
     DECLARE v_status BOOLEAN DEFAULT NULL;
-    SET v_status = has_user(user_id);
+    SET v_status = COALESCE(is_deleted, has_user(user_id));
 
     IF v_status = TRUE THEN
-        SELECT firstname, email, phone, FALSE
-        INTO user_name, user_email, user_phone, is_deleted
-        FROM view_users
-        WHERE id = user_id
-        LIMIT 1;
-    ELSEIF v_status = FALSE THEN
         SELECT firstname, email, phone, TRUE
         INTO user_name, user_email, user_phone, is_deleted
         FROM view_deleted_users
+        WHERE id = user_id
+        LIMIT 1;
+    ELSEIF v_status = FALSE THEN
+        SELECT firstname, email, phone, FALSE
+        INTO user_name, user_email, user_phone, is_deleted
+        FROM view_users
         WHERE id = user_id
         LIMIT 1;
     ELSE
