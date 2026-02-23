cc276377f86de155d19073a6453c6c81adbb4e3f
Malcolmston <mstone@crefeld.org>
2026-02-22 21:22:29 -0500

    feat: add `trg_log_user_update` trigger to log user update actions with old and new data

--- sql/trigger.sql
+++ sql/trigger.sql
@@ -21,4 +21,33 @@ AFTER INSERT ON users
            );
 END;
 
+DROP TRIGGER IF EXISTS trg_log_user_update;
+
+CREATE TRIGGER trg_log_user_update
+AFTER UPDATE ON users
+    FOR EACH ROW BEGIN
+    INSERT INTO logs (user_id, action, table_name, record_id, old_data, new_data)
+    VALUES (
+               NEW.id,
+               'UPDATE',
+               'users',
+               NEW.id,
+               JSON_OBJECT(
+                       'firstname', OLD.firstname,
+                       'lastname',  OLD.lastname,
+                       'email',     OLD.email,
+                       'age',       OLD.age,
+                       'blood',     OLD.blood,
+                       'gender',    OLD.gender
+               ),
+               JSON_OBJECT(
+                       'firstname', NEW.firstname,
+                       'lastname',  NEW.lastname,
+                       'email',     NEW.email,
+                       'age',       NEW.age,
+                       'blood',     NEW.blood,
+                       'gender',    NEW.gender
+               )
+           );
+END;
 
