e05da01dd0e41edce0dfcc8b8ca3c3dfcea58b8f
Malcolmston <mstone@crefeld.org>
2026-02-22 19:42:01 -0500

    feat: add `view_user_roles` view to display users with roles excluding deleted users

---
+++ sql/view.sql
@@ -0,0 +1,27 @@
+-- View: view_user_roles creates a view of users and their roles excluding deleted users and passwords
+CREATE OR REPLACE VIEW view_user_roles AS
+SELECT
+    u.id,
+    u.firstname,
+    u.lastname,
+    u.middlename,
+    u.prefix,
+    u.suffix,
+    u.gender,
+    u.phone,
+    u.location,
+    u.email,
+    u.age,
+    u.status,
+    u.blood,
+    u.extra,
+    u.employid,
+    u.adminid,
+    u.created_at,
+    u.updated_at,
+    u.deleted_at,
+    ur.role,
+    ur.assigned_at
+FROM users u
+         LEFT JOIN user_role ur ON ur.user_id = u.id
+WHERE u.deleted_at IS NULL;
