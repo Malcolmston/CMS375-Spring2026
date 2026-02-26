3a4eaa740c2e65f8ecd21a3e882b11310b112c21
Malcolmston <mstone@crefeld.org>
2026-02-25 20:29:35 -0500

    feat: add `prefix` enum to `account` namespace for title management

---
+++ account/prefix.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace account;
+
+enum prefix: string
+{
+    case Mr = "Mr";
+    case Ms = "Ms";
+    case Mrs = "Mrs";
+    case Miss = "Miss";
+    case Dr = "Dr";
+    case Prof = "Prof";
+    case Mx = "Mx";
+    case Sir = "Sir";
+    case Lady = "Lady";
+    case Rev = "Rev";
+    case Hon = "Hon";
+    case Sgt = "Sgt";
+    case Cpl = "Cpl";
+    case Col = "Col";
+    case Fr = "Fr";
+    case Sr = "Sr";
+}
