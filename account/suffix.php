f1e73d8aba512617c3bca62a74f24111d2fbde36
Malcolmston <mstone@crefeld.org>
2026-02-25 20:31:23 -0500

    feat: add `suffix` enum to `account` namespace for suffix management

---
+++ account/suffix.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace account;
+
+enum suffix: string
+{
+    case Jr  = "Jr";
+    case Sr  = "Sr";
+    case II  = "II";
+    case III = "III";
+    case IV  = "IV";
+    case V   = "V";
+    case PhD = "PhD";
+    case MD  = "MD";
+    case DO  = "DO";
+    case DDS = "DDS";
+    case DMD = "DMD";
+    case JD  = "JD";
+    case Esq = "Esq";
+    case RN  = "RN";
+    case CPA = "CPA";
+    case MBA = "MBA";
+    case MS  = "MS";
+    case MA  = "MA";
+    case BA  = "BA";
+    case BS  = "BS";
+    case OBE = "OBE";
+    case MBE = "MBE";
+    case KBE = "KBE";
+}
