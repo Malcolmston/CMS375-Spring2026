53bda31a583095c839aad4cf169a96b968979e5a
Malcolmston <mstone@crefeld.org>
2026-02-25 20:23:44 -0500

    feat: add password encryption and verification methods to `Account` class

--- account/Account.php
+++ account/Account.php
@@ -9,5 +9,26 @@ use Connect;
  */
 abstract class Account extends Connect
 {
+    /**
+     * Encrypts the provided plain-text password using the bcrypt hashing algorithm.
+     *
+     * @param string $password The plain-text password to be encrypted.
+     * @return string The hashed password.
+     */
+    public static function encryptPassword (string $password): string
+    {
+        return password_hash($password, PASSWORD_BCRYPT, ['cost' => 12]);
+    }
 
+    /**
+     * Verifies if the provided password matches the given hashed value.
+     *
+     * @param string $password The plain-text password to be verified.
+     * @param string $hash The hashed password to compare against.
+     * @return bool Returns true if the password matches the hash, false otherwise.
+     */
+    public static function verifyPassword (string $password, string $hash): bool
+    {
+        return password_verify($password, $hash);
+    }
 }
