c1db85ec11b08cc1aca903666f449fcc3e6aecf8
Malcolmston <mstone@crefeld.org>
2026-03-22 13:28:37 -0400

    feat: add `Billing` class extending `Account` with `login`, `register`, and `loginWithId` methods

---
+++ account/Billing.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace account;
+
+require_once __DIR__ . '/Account.php';
+require_once __DIR__ . '/Employed.php';
+require_once __DIR__ . '/EmployedTrait.php';
+
+class Billing extends Account implements Employed
+{
+
+    /**
+     * @inheritDoc
+     */
+    function login(string $username, string $password): bool
+    {
+        // TODO: Implement login() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    function register(): bool
+    {
+        // TODO: Implement register() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function loginWithId(string $email, string $password, string $id): bool
+    {
+        // TODO: Implement loginWithId() method.
+    }
+}
