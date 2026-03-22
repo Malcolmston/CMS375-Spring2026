f3643e734baf956f3d525242be1a4048c3582143
Malcolmston <mstone@crefeld.org>
2026-03-22 13:22:13 -0400

    style: comment out unused methods in `Employed` interface for cleanup

--- account/Employed.php
+++ account/Employed.php
@@ -7,22 +7,22 @@ interface Employed
     /**
      * Link this employee to an institution with an optional start date.
      */
-    public function joinInstitution(int $institution_id, string $start_date): bool;
+    //public function joinInstitution(int $institution_id, string $start_date): bool;
 
     /**
      * End employment at an institution by setting the end date and marking inactive.
      */
-    public function leaveInstitution(int $institution_id, string $end_date): bool;
+    //public function leaveInstitution(int $institution_id, string $end_date): bool;
 
     /**
      * Get all institutions this employee is currently active at.
      */
-    public function getMyInstitutions(): array;
+    //public function getMyInstitutions(): array;
 
     /**
      * Get all active employees at a given institution.
      */
-    public function getEmployeesByInstitution(int $institution_id): array;
+    //public function getEmployeesByInstitution(int $institution_id): array;
 
     /**
      * Login using the employee's unique ID (employid for staff, adminid for admin)
