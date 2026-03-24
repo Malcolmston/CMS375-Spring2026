c01aa247973d3992249a2a9e143eda486fa81bf9
Malcolmston <mstone@crefeld.org>
2026-03-24 17:52:40 -0400

    feat: implement `fetchEmployeeIds` method in `EmployedTrait` to assign and retrieve `employid` and `adminid`

--- account/EmployedTrait.php
+++ account/EmployedTrait.php
@@ -33,6 +33,24 @@ trait EmployedTrait
         return true;
     }
 
+    protected function fetchEmployeeIds(): void
+    {
+        $col  = ($this->role === role::ADMIN) ? 'adminid' : 'employid';
+        $stmt = $this->getConnection()->prepare("UPDATE users SET $col = UUID() WHERE id = ?");
+        $stmt->bind_param('i', $this->id);
+        $stmt->execute();
+        $stmt->close();
+
+        $stmt = $this->getConnection()->prepare("SELECT employid, adminid FROM users WHERE id = ? LIMIT 1");
+        $stmt->bind_param('i', $this->id);
+        $stmt->execute();
+        $stmt->bind_result($employid, $adminid);
+        $stmt->fetch();
+        $stmt->close();
+        $this->employid = $employid;
+        $this->adminid  = $adminid;
+    }
+
     public static function resolveRole(string $email, string $employid): ?string
     {
         $sql  = "SELECT role FROM view_user_role_pwd
