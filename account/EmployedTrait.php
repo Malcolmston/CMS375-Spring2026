0335c12ef14ca73093c16221e04f2a718372843b
Malcolmston <mstone@crefeld.org>
2026-03-24 18:12:42 -0400

    refactor: remove `loginWithId` method from `EmployedTrait` as it is no longer in use

--- account/EmployedTrait.php
+++ account/EmployedTrait.php
@@ -4,35 +4,6 @@ namespace account;
 
 trait EmployedTrait
 {
-    public function loginWithId(string $email, string $password, string $id): bool
-    {
-        $role = $this->role->value;
-        $sql  = "SELECT id, password FROM view_user_role_pwd
-                 WHERE email    = ?
-                   AND employid = ?
-                   AND role     = ?
-                 LIMIT 1";
-        if (!($stmt = $this->getConnection()->prepare($sql))) {
-            return false;
-        }
-
-        $stmt->bind_param('sss', $email, $id, $role);
-        $stmt->execute();
-        $stmt->bind_result($userId, $hash);
-        if (!$stmt->fetch()) {
-            $stmt->close();
-            return false;
-        }
-        $stmt->close();
-
-        if (!self::verifyPassword($password, $hash)) {
-            return false;
-        }
-
-        $this->id = $userId;
-        return true;
-    }
-
     protected function fetchEmployeeIds(): void
     {
         $col  = ($this->role === role::ADMIN) ? 'adminid' : 'employid';
