105c70879a32d645aa4194836f33a9b019139086
Malcolmston <mstone@crefeld.org>
2026-03-25 17:41:07 -0400

    feat: add `Pharmaceutical` abstract class to encapsulate core attributes and behavior

---
+++ pharmaceutical/Pharmaceutical.php
@@ -0,0 +1,38 @@
+<?php
+namespace pharmaceutical;
+
+require_once __DIR__ . '/../Connect.php';
+
+use Connect;
+use DateTime;
+
+abstract class Pharmaceutical extends Connect
+{
+    protected int $id;
+    protected string $manufacturer ;
+    protected string $name;
+    protected DateTime $createdAt;
+    protected DateTime $updatedAt;
+    protected DateTime $deletedAt;
+
+    protected bool $isDeleted;
+
+    /**
+     * @throws \Exception
+     */
+    public function __construct(string $manufacturer, string $name, string $manufacturing_date)
+    {
+        parent::__construct();
+        $this->manufacturer = $manufacturer;
+        $this->name = $name;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+}
