03af604b3e97cc8940105ee16c0692b2a502c145
Malcolmston <mstone@crefeld.org>
2026-04-10 13:14:02 -0400

    feat: add geocode API integration with CURL for fetching latitude and longitude from address

---
+++ sql/c/point.c
@@ -0,0 +1,73 @@
+#include <curl/curl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+#include "point.h"
+
+struct ResponseBuffer {
+    char *data;
+    size_t size;
+};
+
+static size_t write_callback(void *contents, size_t size, size_t nmemb, void *userp) {
+    size_t realsize = size * nmemb;
+    struct ResponseBuffer *buf = (struct ResponseBuffer *)userp;
+    char *ptr = realloc(buf->data, buf->size + realsize + 1);
+    if (!ptr) return 0;
+    buf->data = ptr;
+    memcpy(&(buf->data[buf->size]), contents, realsize);
+    buf->size += realsize;
+    buf->data[buf->size] = '\0';
+    return realsize;
+}
+
+Point get_point(char *address) {
+    Point p = {0, 0};
+    CURL *curl = curl_easy_init();
+
+    if (curl) {
+        const char *api_key = getenv("GEOCODE_API_KEY");
+        if (!api_key) {
+            fprintf(stderr, "GEOCODE_API_KEY not set\n");
+            curl_easy_cleanup(curl);
+            return p;
+        }
+
+        char *encoded = curl_easy_escape(curl, address, 0);
+        char url[1024];
+        snprintf(url, sizeof(url),
+                 "https://api.geocode.farm/forward/?key=%s&addr=%s",
+                 api_key, encoded);
+        curl_free(encoded);
+
+        struct ResponseBuffer response = {0};
+        response.data = malloc(1);
+        response.size = 0;
+
+        curl_easy_setopt(curl, CURLOPT_URL, url);
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
+
+        CURLcode res = curl_easy_perform(curl);
+
+        if (res != CURLE_OK) {
+            fprintf(stderr, "Request failed: %s\n", curl_easy_strerror(res));
+        } else if (response.data) {
+            char *lat_str = strstr(response.data, "\"lat\": \"");
+            char *lon_str = strstr(response.data, "\"lon\": \"");
+            if (lat_str && lon_str) {
+                lat_str += strlen("\"lat\": \"");
+                lon_str += strlen("\"lon\": \"");
+                p.lat = atof(lat_str);
+                p.lon = atof(lon_str);
+            }
+        }
+
+        free(response.data);
+        curl_easy_cleanup(curl);
+    }
+
+    return p;
+}
