0cbda12e84f71003adc53de77fd4231297da6837
Malcolmston <mstone@crefeld.org>
2026-04-11 11:48:08 -0400

    feat: add main program to convert address to POINT and print latitude/longitude

---
+++ sql/c/main.c
@@ -0,0 +1,14 @@
+//
+// Created by Malcolm Stone on 4/10/26.
+//
+#include <stdio.h>
+#include "point.h"
+
+int main() {
+    char* address = "1600 Amphitheatre Parkway, Mountain View, CA";
+
+    Point point = get_point(address);
+    printf("Latitude: %.6f, Longitude: %.6f\n", point.lat, point.lon);
+
+    return 0;
+}
