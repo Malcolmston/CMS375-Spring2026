177358f8924f1b2d9f440251503243b3d9fdd447
Malcolmston <mstone@crefeld.org>
2026-04-12 16:53:01 -0400

    feat: add quick_search function and SearchResults struct for point querying

--- sql/c/point.h
+++ sql/c/point.h
@@ -1,12 +1,20 @@
 #ifndef POINT_H
 #define POINT_H
 
+#define QUICK_SEARCH_LIMIT 5
+
 typedef struct {
     double lat;
     double lon;
     char display_name[512];
 } Point;
 
-Point get_point(char *address);
+typedef struct {
+    Point results[QUICK_SEARCH_LIMIT];
+    int count;
+} SearchResults;
+
+Point         get_point(char *address);
+SearchResults quick_search(char *address);
 
 #endif
